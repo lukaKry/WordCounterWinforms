837eb2402e17e0549bac4d9328050ca55f21f23e
lukaKry <kryszak.lukasz@gmail.com>
2021-01-06 18:22:57 +0100

    first commit

---
+++ WordCounterWinforms/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace WordCounterWinforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_loadFile = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_split = new System.Windows.Forms.Button();
+            this.btn_clear2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_erase = new System.Windows.Forms.Button();
+            this.btn_Sort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Load the file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. Split the file into Words";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(698, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3. Count the frequency of Each Word";
+            // 
+            // btn_loadFile
+            // 
+            this.btn_loadFile.Location = new System.Drawing.Point(12, 62);
+            this.btn_loadFile.Name = "btn_loadFile";
+            this.btn_loadFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_loadFile.TabIndex = 5;
+            this.btn_loadFile.Text = "Load File";
+            this.btn_loadFile.UseVisualStyleBackColor = true;
+            this.btn_loadFile.Click += new System.EventHandler(this.btn_loadFile_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(93, 62);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 6;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(235, 318);
+            this.textBox1.TabIndex = 7;
+            // 
+            // btn_split
+            // 
+            this.btn_split.Location = new System.Drawing.Point(315, 61);
+            this.btn_split.Name = "btn_split";
+            this.btn_split.Size = new System.Drawing.Size(75, 23);
+            this.btn_split.TabIndex = 8;
+            this.btn_split.Text = "Split";
+            this.btn_split.UseVisualStyleBackColor = true;
+            this.btn_split.Click += new System.EventHandler(this.btn_split_Click);
+            // 
+            // btn_clear2
+            // 
+            this.btn_clear2.Location = new System.Drawing.Point(396, 62);
+            this.btn_clear2.Name = "btn_clear2";
+            this.btn_clear2.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear2.TabIndex = 9;
+            this.btn_clear2.Text = "Clear";
+            this.btn_clear2.UseVisualStyleBackColor = true;
+            this.btn_clear2.Click += new System.EventHandler(this.btn_clear2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(329, 133);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 316);
+            this.listBox1.TabIndex = 10;
+            // 
+            // btn_erase
+            // 
+            this.btn_erase.Location = new System.Drawing.Point(12, 91);
+            this.btn_erase.Name = "btn_erase";
+            this.btn_erase.Size = new System.Drawing.Size(156, 23);
+            this.btn_erase.TabIndex = 11;
+            this.btn_erase.Text = "Erase polish letters";
+            this.btn_erase.UseVisualStyleBackColor = true;
+            this.btn_erase.Click += new System.EventHandler(this.btn_erase_Click);
+            // 
+            // btn_Sort
+            // 
+            this.btn_Sort.Location = new System.Drawing.Point(477, 61);
+            this.btn_Sort.Name = "btn_Sort";
+            this.btn_Sort.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sort.TabIndex = 12;
+            this.btn_Sort.Text = "Sort";
+            this.btn_Sort.UseVisualStyleBackColor = true;
+            this.btn_Sort.Click += new System.EventHandler(this.btn_Sort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 578);
+            this.Controls.Add(this.btn_Sort);
+            this.Controls.Add(this.btn_erase);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_clear2);
+            this.Controls.Add(this.btn_split);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_loadFile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_loadFile;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_split;
+        private System.Windows.Forms.Button btn_clear2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_erase;
+        private System.Windows.Forms.Button btn_Sort;
+    }
+}
+
---
+++ WordCounterWinforms/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordCounterWinforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_loadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFile.FileName;
+                textBox1.Text = File.ReadAllText(filePath, Encoding.Default);
+            }
+
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void btn_clear2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void btn_split_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string[] textSplit = text.Split(' ', ',', '.', '?', '!', '-');
+
+            foreach (string word in textSplit)
+            {
+                if (!listBox1.Items.Contains(word))
+                {
+                    listBox1.Items.Add(word);
+                }
+            }
+        }
+
+        private void btn_erase_Click(object sender, EventArgs e)
+        {
+            /*loading another file 
+            string text1 = "";
+            OpenFileDialog noForaignLettersFile = new OpenFileDialog();
+            if (noForaignLettersFile.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = noForaignLettersFile.FileName;
+                text1 = File.ReadAllText(filePath);
+            }
+            */
+
+
+            /* attempt Replace method - does it work ?
+            text1.Replace("w","___");
+            textBox2.Text = text1;
+            */
+
+            //swaping polish language special symbol using for loop
+
+           string text1 = textBox1.Text;
+           char[] letters = text1.ToCharArray();
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                switch (letters[i])
+                {
+                    case 'ł': letters[i] = 'l'; break;
+                    case 'Ł': letters[i] = 'L'; break;
+                    case 'ą': letters[i] = 'a'; break;
+                    case 'Ą': letters[i] = 'A'; break;
+                    case 'ć': letters[i] = 'c'; break;
+                    case 'Ć': letters[i] = 'C'; break;
+                    case 'ę': letters[i] = 'e'; break;
+                    case 'Ę': letters[i] = 'E'; break;
+                    case 'ń': letters[i] = 'n'; break;
+                    case 'Ń': letters[i] = 'N'; break;
+                    case 'ó': letters[i] = 'o'; break;
+                    case 'Ó': letters[i] = 'O'; break;
+                    case 'ś': letters[i] = 's'; break;
+                    case 'Ś': letters[i] = 'S'; break;
+                    case 'ź': letters[i] = 'z'; break;
+                    case 'Ź': letters[i] = 'Z'; break;
+                    case 'ż': letters[i] = 'z'; break;
+                    case 'Ż': letters[i] = 'Z'; break;
+                    default:
+                        break;
+                    
+                }
+            }
+            textBox1.Clear();
+            string textNoForaignLetters = new string(letters);
+            textBox1.Text = textNoForaignLetters;
+            
+            
+
+
+
+        }
+
+        private void btn_Sort_Click(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true;
+        }
+    }
+}
